664921b9ae77f6be3db7f87b6e1d730912946d2d
Luke Bechtel <lukebechtel4@gmail.com>
2015-12-11 14:04:44 -0500

    add comments, header, argument parsing

--- get_all_tables.rb
+++ get_all_tables.rb
@@ -1,16 +1,43 @@
+#!/usr/bin/ruby 
+#Title: html_table_to_csv.rb
+#Author:           Luke Bechtel
+#Organization:     independent work    
+#Description: Given an html file containing a table with id="targeted_table",
+#   Converts the table to a new .csv file.
+#   WARNING: WILL OVERWRITE FILE 
+
 require 'nokogiri'
 require 'pp'
 require 'csv'
 
-html_txt = File.open("index.html", "r").read()
+USAGE="USAGE: ruby html_table_to_csv.rb [input_filepath] [output_csv_filepath]"
+OUTPUT_FILE_EXISTS="Output File '%s' already exists! Please specify nonexisting file."
+
+#Argument Parsing
+if ARGV.count != 2
+  $stderr.puts USAGE
+  exit(0)
+end
+
+infilepath = ARGV[0]
+outfilepath = ARGV[1]
+
+if File.exists?(outfilepath)
+  $stderr.puts OUTPUT_FILE_EXISTS % (outfilepath)
+  exit(0)
+end
+
+#################################################################
+#Business logic starts here.
+#
+
+html_txt = File.open(infilepath, "r").read()
 
 doc = Nokogiri::HTML(html_txt)
 nk_rows = doc.xpath('//table[@id="targeted_table"]/tbody/tr')
 
-r = 0
-c = 0
-
-#create an array of arrays,
+#Create an array of arrays,
+#With elements of each row as the inner arrays.
 #Form: [[r0c0, r0c1...],[r1c0, r1c1,..],..]
 rows = nk_rows.collect do |nk_row|
   nk_cols = nk_row.xpath('td')
@@ -22,13 +49,10 @@ rows = nk_rows.collect do |nk_row|
 end
 
 #Output CSV File
-CSV.open("data.csv", "wb") do |csv|
+CSV.open(outfilepath, "wb") do |csv|
   rows.each do |r|
-    $stderr.puts r.to_s
+    #$stderr.puts r.to_s
     csv << r
   end
 end
-
-$stderr.puts "============================="
-pp rows
 
